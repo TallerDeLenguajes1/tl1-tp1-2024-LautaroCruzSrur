a714e176cb0f3cd9b3255541caeb6a6a101de51a
Lautaro Cruz <lautaro.cruzsrur@gmial.com>
2024-03-26 12:11:46 -0300

    tp1

--- tp1_1.cpp
+++ tp1_1.cpp
@@ -1,6 +1,18 @@
 #include <stdio.h>
 
 int main(){
-    printf("Hola mundo \n");
+    int variable = 20;
+    int *ptr = &variable;
+
+    printf("1) El contenido del puntero: %d\n" , *ptr);
+
+    printf("2) La direccion de memoria almacenada por el puntero: %p\n" , (void*)ptr);
+
+    printf("3)La direccion de memoria de la variable: %p\n" , (void*)&variable);
+
+    printf("4) La direccion de memoria del puntero: %p\n" , (void*)&ptr);
+
+    printf("5) el tamaño de memoria utilizado por esa variable usando la función sizeof()\n" , sizeof(variable));
+    
     return 0;
 }
